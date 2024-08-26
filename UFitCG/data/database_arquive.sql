08e2889501924c0d173dc7929d2a1f334deced14
Eurico149 <eurico.gabriel.vasconcelos.pereira@ccc.ufcg.edu.br>
2024-08-26 11:30:48 -0300

    modificacoes no bd

--- UFitCG/data/database_arquive.sql
+++ UFitCG/data/database_arquive.sql
@@ -6,62 +6,47 @@ CREATE TABLE assinatura(
         valor_anual REAL,
         desconto INTEGER,
         aulas_gratis INTEGER,
-        acesso VARCHAR(23)
+        acesso VARCHAR(23),
+        CHECK (aulas_gratis >= 0),
+        CHECK (desconto >= 0),
+        CHECK (length(sigla) = 3)
 );
 
+-- Formato data_nascimento: dd-mm-aaaa 
 CREATE TABLE usuario(
-        matricula CHAR(9) PRIMARY KEY,
-        senha VARCHAR(8) NOT NULL,
+        usr VARCHAR(40) PRIMARY KEY,
+        senha CHAR(8) NOT NULL,
         tipo_usr CHAR(3) NOT NULL,
         nome VARCHAR(50) NOT NULL,
         data_nascimento VARCHAR(10),
-
-        CHECK (tipo_usr in ('ADM', 'PER', 'CLI'))
-);
-
--- Formato dara_nascimento: dd-mm-aaaa
-CREATE TABLE cliente(
-        matricula CHAR(9) PRIMARY KEY,
-        nome VARCHAR(50) NOT NULL,
-        data_nascimento VARCHAR(10),
-        tipo_assinatura CHAR(3) NOT NULL,
+        tipo_assinatura CHAR(3),
+        salario REAL,
+        CHECK (salario >= 0),
+        CHECK (length(tipo_assinatura) = 3),
+        CHECK (length(senha) = 8),
+        CHECK (tipo_usr in ('ADM', 'PER', 'CLI')),
         FOREIGN KEY(tipo_assinatura) REFERENCES assinatura(sigla)
-);
-
--- Formato dara_nascimento: dd-mm-aaaa
-CREATE TABLE personal(
-        matricula CHAR(9) PRIMARY KEY,
-        nome VARCHAR(50) NOT NULL,
-        data_nascimento VARCHAR(10),
-        salario REAL
-);
-
--- Formato dara_nascimento: dd-mm-aaaa
-CREATE TABLE admin(
-        matricula CHAR(9) PRIMARY KEY,
-        nome VARCHAR(50) NOT NULL ,
-        data_nascimento VARCHAR(10),
-        salario REAL
 );
 
 -- Formato data: dd-mm-aaaa
 CREATE TABLE vendas_assinatura(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        matricula CHAR(9) NOT NULL,
+        usr VARCHAR(40) NOT NULL,
         tipo_assinatura CHAR(3) NOT NULL,
         tipo_parcela CHAR(1) NOT NULL,
         parcelas_pagas INTEGER NOT NULL,
         data_inicio CHAR(10) NOT NULL,
         CHECK (tipo_parcela IN ('M', 'S', 'A')),
+        CHECK (length(tipo_assinatura) = 3),
         FOREIGN KEY(tipo_assinatura) REFERENCES assinatura(sigla),
-        FOREIGN KEY(matricula) REFERENCES cliente(matricula)
+        FOREIGN KEY(usr) REFERENCES usuario(usr)
 );
 
 
 -- Formato categorias: bebida suplemento barra_proteina,...
 CREATE TABLE loja(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        nome VARCHAR(60),
+        nome VARCHAR(60) NOT NULL,
         valor REAL NOT NULL,
         descricao VARCHAR(250),
         categorias VARCHAR(50),
@@ -73,60 +58,62 @@ CREATE TABLE loja(
 CREATE TABLE vendas_loja(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         produtos TEXT,
-        matricula_usr CHAR(9),
-        data VARCHAR(16),
-        valor_total REAL,
-        FOREIGN KEY(matricula_usr) REFERENCES cliente(matricula)
+        usr VARCHAR(40) NOT NULL,
+        data_horario VARCHAR(16),
+        valor_total REAL NOT NULL,
+        CHECK (valor_total >= 0),
+        FOREIGN KEY(usr) REFERENCES usuario(usr)
 );
 
 -- Formato exercicios: nome_exercicio seriesXrepeticoes,...
 CREATE TABLE ficha_treino(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        matricula_cli CHAR(9),
-        matricula_per CHAR(9),
+        usr_cli VARCHAR(40) NOT NULL,
+        usr_per VARCHAR(40) NOT NULL,
         exercicios TEXT,
         observacoes VARCHAR(250),
-        FOREIGN KEY(matricula_cli) REFERENCES cliente(matricula),
-        FOREIGN KEY(matricula_per) REFERENCES personal(matricula)
+        FOREIGN KEY(usr_cli) REFERENCES usuario(usr),
+        FOREIGN KEY(usr_per) REFERENCES usuario(usr)
 );
 
 CREATE TABLE suporte(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        matricula_cli CHAR(9) NOT NULL,
-        matricula_fun CHAR(9) NOT NULL,
+        usr_cli VARCHAR(40) NOT NULL,
+        usr_fun VARCHAR(40) NOT NULL,
         comentario TEXT,
-        FOREIGN KEY(matricula_cli) REFERENCES cliente(matricula)
+        FOREIGN KEY(usr_cli) REFERENCES usuario(usr),
+        FOREIGN KEY(usr_fun) REFERENCES usuario(usr)
 );
 
 -- Formato data: dd/mm/aaaa
 -- formato avalicao: parte_corpo medida,gordura_corporal num%,...
 CREATE TABLE avaliacao_fisica(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        matricula_cli CHAR(9),
-        matricula_per CHAR(9),
+        usr_cli VARCHAR(40) NOT NULL,
+        usr_per VARCHAR(40) NOT NULL,
         avalicao TEXT,
         observacoes TEXT,
-        data VARCHAR(10),
-        FOREIGN KEY(matricula_cli) REFERENCES cliente(matricula),
-        FOREIGN KEY(matricula_per) REFERENCES personal(matricula)
+        data_ava VARCHAR(10),
+        FOREIGN KEY(usr_cli) REFERENCES usuario(usr),
+        FOREIGN KEY(usr_per) REFERENCES usuario(usr)
 );
 
 -- Formato de data: dd-mm--aa horainicio,horafim EX: 23-08-2004 12:30,13:00
 CREATE TABLE aula_extra(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        materia VARCHAR(40),
-        matricula_per CHAR(9),
-        data VARCHAR(22) NOT NULL,
+        materia VARCHAR(40) NOT NULL,
+        usr_per VARCHAR(40) NOT NULL,
+        data_horario VARCHAR(22),
         limite INTEGER,
-        FOREIGN KEY(matricula_per) REFERENCES personal(matricula)
+        CHECK (limite > 0),
+        FOREIGN KEY(usr_per) REFERENCES usuario(usr)
 );
 
 CREATE TABLE clientes_aulas(
-        matricula_cli CHAR(9),
+        usr_cli VARCHAR(40) NOT NULL,
         id_aula INTEGER,
-        FOREIGN KEY(matricula_cli) REFERENCES cliente(matricula),
+        FOREIGN KEY(usr_cli) REFERENCES usuario(usr),
         FOREIGN KEY(id_aula) REFERENCES aula_extra(id)
 );
 
-INSERT INTO usuario(matricula, senha, tipo_usr) VALUES ('111111111', '12345678', 'ADM');
-INSERT INTO admin(matricula, nome, data_nascimento,salario) VALUES ('111111111', 'Eurico Gabriel Vasconcelos Pereira', '13/02/2000', 3500.00);
+INSERT INTO usuario(usr, senha, tipo_usr, nome, data_nascimento,salario) VALUES ('eurico', '12345678', 'ADM', 'Eurico Gabriel Vasconcelos Pereira', '13/02/2000', 3500.00);
