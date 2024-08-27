e30a3a86793d95ec2327735ed7130e6e7c203aba
mcmaiaa <renaldo.santos.franca@ccc.ufcg.edu.br>
2024-08-26 21:06:24 -0300

    CadastraUsuario

--- UFitCG/app/Main.hs
+++ UFitCG/app/Main.hs
@@ -4,6 +4,7 @@ import Database.SQLite.Simple
 import Data.String (fromString)
 
 data Produto = Produto String Float String String
+data Usuario = Usuario String String String String String String Float
 
 cadastroProduto :: Connection -> Produto -> IO ()
 cadastroProduto conn (Produto nome valor descricao categorias) = do
@@ -11,10 +12,18 @@ cadastroProduto conn (Produto nome valor descricao categorias) = do
     execute conn query (nome, valor, descricao, categorias)
     putStrLn "Produto inserido com sucesso!"
 
+cadastraUsuatio :: Connection -> Usuario -> IO ()
+cadastraUsuatio  conn (Usuario usr senha tipo_usr nome data_nascimento tipo_assinatura salario) = do
+    let query = fromString "INSERT INTO usuario(usr, senha, tipo_usr, nome, data_nascimento, tipo_assinatura, salario) VALUES(?,?,?,?,?,?,?);"
+    execute conn query (usr, senha, tipo_usr, nome, data_nascimento, tipo_assinatura, salario)
+    putStrLn $ "Usuario: " ++ nome ++ " cadastrado com sucesso."
+
 -- Função principal
 main :: IO ()
 main = do
     conn <- open "data/DataBase.db"
     let prod = Produto "Abacate" 12.50 "Fruta fresca" "Alimentos"
     cadastroProduto conn prod
+    let usr = Usuario "RenaldoF20" "87654321" "ADM" "Renaldo Franca" "20/10/2003" "   " 15000
+    cadastraUsuatio conn usr
     close conn
