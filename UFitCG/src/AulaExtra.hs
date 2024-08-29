409709a2ce0e8b0fc2409778f3daf0b2f3aaffbd
Clara Maia <mcsilvamaia@gmail.com>
2024-08-29 17:46:21 -0300

    AulaExtra

--- UFitCG/src/AulaExtra.hs
+++ UFitCG/src/AulaExtra.hs
@@ -1,43 +1,39 @@
 {-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE OverloadedStrings #-}
 module AulaExtra (cadastraAula) where
 
 import Data.String (fromString)
-import Database.SQLite.Simple 
-import Database.SQLite.Simple.FromRow
-import Database.SQLite.Simple.ToField (Only(..)) 
+import Database.SQLite.Simple
 
-data AulaExtra = AulaExtra Int String String String Int
+data AulaExtra = AulaExtra String String String Int
 
-cadastraAula :: Int -> String -> String -> String -> String -> Int -> IO String
-cadastraAula id modalidade urs_per data_horario limite = do
+cadastraAula :: String -> String -> String -> Int -> IO String
+cadastraAula materia usr_per data_horario limite = do
     conn <- open "data/DataBase.db"
     aula <- verificaExistenciaAula conn data_horario
-    personal <- verificaExistenciaPersonal conn urs_per
-    close conn
-    resultado <- verificaCondicoes conn aula personal limite id modalidade urs_per data_horario
+    personal <- verificaExistenciaPersonal conn usr_per
+    resultado <- verificaCondicoes conn aula personal limite materia usr_per data_horario
     close conn
     return resultado
 
-verificaCondicoes :: Connection -> Int -> Int -> Int -> Int -> String -> String -> String -> IO String
-verificaCondicoes conn aula personal limite id modalidade urs_per data_horario
+verificaCondicoes :: Connection -> Int -> Int -> Int -> String -> String -> String -> IO String
+verificaCondicoes conn aula personal limite materia usr_per data_horario
     | personal == 0  = return "Usuario Invalido!"
     | aula >= 1      = return "O horário está ocupado!"
     | limite <= 0    = return "Limite Inválido"
-    | otherwise      = do
-        let aulaExtra = AulaExtra id modalidade urs_per data_horario limite
+    | length data_horario /= 22 = return "Horario Invalido"
+    | otherwise = do
+        let aulaExtra = AulaExtra materia usr_per data_horario limite
+        conn <- open "data/DataBase.db"
         insertAula conn aulaExtra
+        close conn
         return "Aula Cadastrada Com Sucesso!"
 
 insertAula :: Connection -> AulaExtra -> IO ()
-insertAula conn aulaExtra = do
-    let query = "INSERT INTO aula_extra(id, modalidade, urs_per, data_horario, limite) VALUES(?,?,?,?,?);"
-        params = ( idAula aulaExtra
-                 , modalidade aulaExtra
-                 , usrPer aulaExtra
-                 , dataHorario aulaExtra
-                 , limite aulaExtra )
-    execute conn query params
-    putStrLn "Aula inserida com sucesso."
+insertAula conn (AulaExtra materia usr_per data_horario limite) = do
+    let query = fromString "INSERT INTO aula_extra(materia, usr_per, data_horario, limite) VALUES(?,?,?,?);"
+    execute conn query (materia, usr_per, data_horario, limite)
+    return ()
 
 verificaExistenciaAula :: Connection -> String -> IO Int
 verificaExistenciaAula conn dataH = do
@@ -46,5 +42,5 @@ verificaExistenciaAula conn dataH = do
 
 verificaExistenciaPersonal :: Connection -> String -> IO Int
 verificaExistenciaPersonal conn personal = do
-    [Only count] <- query conn "SELECT COUNT(*) FROM usuario WHERE usr = ? AND (tipo_usr = 'per' OR tipo_usr = 'adm') " (Only personal)
+    [Only count] <- query conn "SELECT COUNT(*) FROM usuario WHERE usr = ? AND (tipo_usr = 'PER' OR tipo_usr = 'ADM') " (Only personal)
     return count
